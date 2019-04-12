06e3983c9bdcb72b5b4c008ad40a064891ec668b
Luke Fuller <jlfuller007@gmail.com>
2019-04-11 20:43:02 -0400

    Updates

---
+++ precache-manifest.506887689e91427f8cbfc096da8ce7ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "f2dceffd9a968c56f5a1",
+    "url": "/clickyGame/static/js/main.37b77376.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f2dceffd9a968c56f5a1",
+    "url": "/clickyGame/static/css/main.a954984c.chunk.css"
+  },
+  {
+    "revision": "a24b458a77ef7954b553f5397f7bae53",
+    "url": "/clickyGame/index.html"
+  }
+];
